e2d417f5175d37f2279b3a8061b4bcf9d9e7c614
hannnbit <haun24ne@gmail.com>
2021-12-30 16:44:45 +0900

    starForce_temp

--- Maple_Star_Probability_Verification/Maple_Star_Probability_Verification/header.h
+++ Maple_Star_Probability_Verification/Maple_Star_Probability_Verification/header.h
@@ -2,6 +2,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <windows.h>
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 
 int GetItemLevel()
@@ -20,7 +21,7 @@ int GetCurrentStarLevel()
 	char CurrentStarLevel[100] = { 0 };
 	int temp = scanf("%s", CurrentStarLevel);
 	return atoi(CurrentStarLevel);
-}
+}	 
 int GetGoalStarLevel()
 {
 	printf("Input goal of your star force item : ");
@@ -29,11 +30,49 @@ int GetGoalStarLevel()
 	int temp = scanf("%s", GoalStarLevel);
 	return atoi(GoalStarLevel);
 }
-int GetExpectedValue()
+int ChkStarCatch()
 {
-	printf("Input starforce expected value : ");
+	printf("Input 'O' if you use starcatch, or not intpur 'X' : ");
 
-	char expected[100] = { 0 };
-	int temp = scanf("%s", expected);
-	return atoi(expected);
+	char starcatch[100] = { 0 };
+	int temp = scanf("%s",starcatch);
+	_strupr(starcatch);
+
+	return starcatch[0] == 'O' ? 1 : 0;
+}
+
+double GetCalculatedProbability(int nowLevel,int starCatch)
+{
+	double revisionPro = starCatch ? 1.05 : 1;
+	int starPro = nowLevel > 13 ? 30 : 100 - (nowLevel) * 5;
+	starPro = nowLevel < 3 ? starPro - 5 : starPro;
+
+	return starPro * revisionPro;
+}
+int GetNeededMeso(int itemLevel, int currentStar)
+{
+	int L = itemLevel * itemLevel * itemLevel;
+	int S = currentStar;
+	double meso = 0;
+
+	if (currentStar < 10)
+	{
+		meso = 1000 + L * (S + 1) / 25;
+	}
+	else if (currentStar < 15)
+	{
+		meso = 1000 + L * pow((S + 1), 2.7) / 400;
+	}
+	else
+	{
+		meso = 1000 + L * pow((S + 1), 2.7) / 200;
+	}
+	return (int)meso;
+}
+int GetExProMeso(int star, double sucpro, double meso)
+{
+	if (star < 11)
+		return (int)(100 / sucpro * meso);
+	else
+		return 0;
 }
--- Maple_Star_Probability_Verification/Maple_Star_Probability_Verification/main.c
+++ Maple_Star_Probability_Verification/Maple_Star_Probability_Verification/main.c
@@ -3,7 +3,25 @@
 void main()
 {
 	int itemLevel = GetItemLevel();
-	int CurrentStarLevel = GetCurrentStarLevel();
-	int GoalStarLevel = GetGoalStarLevel();
-	int Expected = GetExpectedValue();
+	int currentStarLevel = GetCurrentStarLevel();
+	int goalStarLevel = GetGoalStarLevel();
+	int starCatch = ChkStarCatch();
+	
+	int starGap = goalStarLevel - currentStarLevel;
+	long long totalMeso = 0;
+	for (int i = 0; i < starGap; i++)
+	{
+		double sucPro = GetCalculatedProbability(currentStarLevel + i, starCatch);
+		printf("\n%d성 : %.1lf", currentStarLevel + i + 1, sucPro);
+
+		int meso = GetNeededMeso(itemLevel, currentStarLevel+i);
+		printf("\t필요메소 : %d", meso);
+
+		int exMeso = GetExProMeso(currentStarLevel+i,sucPro,meso);
+		printf("\t 기댓값메소 : %d", exMeso);
+
+		totalMeso += exMeso;
+		printf("\t 총 메소 : %lld", totalMeso);
+	}
+	printf("\n\n\n\n\n\n\n\n\n\n\n");
 }
